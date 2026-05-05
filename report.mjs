a65b00e5d2aef48f03c9b8e7ea67f85d8d4c8df5
Mark-El <phellearnex@gmail.com>
2026-05-05 04:50:03 +0000

    feat: finance report

---
+++ report.mjs
@@ -0,0 +1,48 @@
+import { transactions, filterTransactions, monthMap } from "./transactions.mjs";
+
+/**
+ * {
+    month: 
+    totalIncome:
+    totalExpense:
+    dailyAverageSpend:
+    netBalance
+ * }
+ */
+
+function getMonthlySummary(month) {
+    let monthTransactions = filterTransactions({month: month});
+    let monthIncomes = monthTransactions.filter(transaction => transaction.transactionType == "INCOME");
+    let totalMonthIncome = monthIncomes.reduce((sum, transaction) => sum + transaction.amount, 0);
+    let monthExpenses = monthTransactions.filter(transaction => transaction.transactionType == "EXPENSE");
+    let totalMonthExpense = monthExpenses.reduce((sum, transaction) => sum + transaction.amount, 0);
+    let year = new Date().getFullYear();
+    let dailyAverageSpend = totalMonthExpense / new Date(year, monthMap[month] +1, 0).getDate();
+    let netBalance = totalMonthIncome - totalMonthExpense;
+
+    return {
+        month,
+        totalMonthIncome,
+        totalMonthExpense,
+        dailyAverageSpend,
+        netBalance,
+    }
+}
+
+function getCategoryBreakdown(month) {
+    let monthTransactions = filterTransactions({month: month});
+    let monthExpenses = monthTransactions.filter(transaction => transaction.transactionType == "EXPENSE");
+    const categoryBreakdown = {};
+
+    monthExpenses.forEach(transaction => {
+        if (categoryBreakdown[transaction.category]) {
+            categoryBreakdown[transaction.category] += transaction.amount
+        } else {
+            categoryBreakdown[transaction.category] = transaction.amount
+        }
+    });
+
+    return categoryBreakdown;
+}
+
+export { getMonthlySummary, getCategoryBreakdown }
