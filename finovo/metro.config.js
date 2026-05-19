03a529bdec2aed2ab151c176f18e08bb3832d4e8
Mark-El <phellearnex@gmail.com>
2026-05-19 14:53:37 +0000

    feat: enhance metro.config.js to handle dynamic imports for @opentelemetry/api

--- finovo/metro.config.js
+++ finovo/metro.config.js
@@ -2,4 +2,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Prevent @opentelemetry/api's dynamic import() from breaking the Hermes build
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === '@opentelemetry/api') {
+    return {
+      filePath: require.resolve('@opentelemetry/api'),
+      type: 'sourceFile',
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
